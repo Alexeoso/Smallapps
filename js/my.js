bf654e68a27a3c8c825bdca294c47fed0a582b21
Alexeoso <72703132+Alexeoso@users.noreply.github.com>
2022-03-01 15:26:35 +0300

    01032022/1526

--- js/my.js
+++ js/my.js
@@ -1,135 +1,3 @@
-// "use strict"
-
-// const isMobile = {
-//     Android: function () {
-//         return navigator.userAgent.match(/Android/i);
-//     },
-//     BlackBerry: function () {
-//         return navigator.userAgent.match(/BlackBerry/i);
-//     },
-//     IOS: function () {
-//         return navigator.userAgent.match(/iPhone|Ipad|iPod/i);
-//     },
-//     Opera: function () {
-//         return navigator.userAgent.match(/Opera Mini/i);
-//     },
-//     Windows: function () {
-//         return navigator.userAgent.match(/IEMobile/i);
-//     },
-//     any: function () {
-//         return ( 
-//             isMobile.Android() ||
-//             isMobile.BlackBerry() ||
-//             isMobile.IOS() ||
-//             isMobile.Opera() ||
-//             isMobile.Windows());
-        
-//     },
-// };
- 
-// if (isMobile.any()) {
-//     document.body.classList.add('_touch');
-// } else {
-//     document.body.classList.add('_pc');
-// };
-
-// const dob = document.querySelectorAll("li");
-// const t = window.innerWidth;
-// const ttt = document.body.previousElementSibling;
-// const tt = document.body.nextElementSibling;
-// const t = document.body.parentElement;
-
-// console.log(dob[0]);
-// while (dob.length) {
-//     console.log(dob.length);
-// }
-/*const t = {
-    one: function () {
-                return 1+1==2;
-    },
-    two: function () {
-                return 1+1==3;
-    },
-    three: function () {
-                return ( t.one() || t.two());
-    },
-    
-};
-var www = t.three();
-
-
-console.log(www);
-if ( www ) {
-    console.log("Условие сработало");
-} else {console.log("Условие не сработало");}; */
-
-// var tr = document.getElementById('choice__suggestions');
-
-// num = 8;
-// while (num) {console.log(num); num--};
-// one: for (let num = 0; num < 3; num++) {
-//     for (let size = 0; size < 3; size++) {
-//         if (size == 3) { break one};
-//         console.log(size);
-//     };
-// };
-// const elems = document.querySelectorAll('li');
-// console.log(elems[2]);
-//  // Добавить класс
-//  block__text.classList.add('one-block__classe');
-
-//  var qq = block__text.className;
-//  console.log(qq);
-
-
-
-// const block__text = document.querySelector('.uull');
-// block__text.style.cssText = `
-//     width: 470px;
-//     color: red;
-//     opacity: 0.4;
-// `;
-// block__text.style.paddingTop = "110px";
-// console.log(block__text.style.paddingTop);
-// block__text.0 = false;
-// console.log(block__text.0);
-
-//_Домашка + + + +
-
-// const block__text = document.querySelector('.uull');
-// console.log(block__text.dataset.sayHi);
-
-// const block__text = document.querySelector('.uull');
-// var r = block__text.lastElementChild;
-
-// const block__text = document.getElementsByClassName('uull');
-// const block__text = document.querySelectorAll('.uull');
-// console.log(block__text);
-
-// const block__text = document.querySelector('.uull');
-// block__text.insertAdjacentHTML(
-//     'beforeend',
-//     `<li>Текст</li>`
-// );
-// console.log(block__text.innerHTML);
-
-// const wq = document.documentElement;
-// var ww = wq.clientWidth;
-// var www = wq.clientHeight;
-// var ww = window.innerWidth;
-// var www = window.innerHeight;
-// var ww = Math.max(
-//     document.body.scrollWidth, document.body.offsetWidth, document.body.clientWidth,
-//     document.documentElement.scrollWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth
-// );
-// var www = Math.max(
-//     document.body.scrollHeight, document.body.offsetHeight, document.body.clientHeight,
-//     document.documentElement.scrollHeight, document.documentElement.offsetHeight, document.documentElement.clientHeight
-// );
-// var ww = window.pageYOffset;
-// var www = window.pageXOffset;
-// console.log(ww, www);
-
 var clickSuggestion1 = document.querySelector(".choice__slider_1");
 var clickSuggestion2 = document.querySelector(".choice__slider_2");
 var clickSuggestion3 = document.querySelector(".choice__slider_3");
@@ -154,8 +22,8 @@ var suggestionEmage5 = document.querySelector(".two-block__suggestion-info_5");
 var choiceTitle = document.querySelectorAll(".choice__title");//Имя в предложении
 
 clickSuggestion1.style.opacity = "1";
-choiceimg1.style.backgroundImage= "url(../img/svg/a-heart-green.svg)";
-choiceTitle[0].style.color = "#000";
+choiceimg1.style.backgroundImage= "url(./img/svg/a-heart-green.svg)";
+// choiceTitle[0].style.color = "#000";
 suggestionEmage1.style.display = "block";
 //  Включены по уммолчанию
 
@@ -166,11 +34,11 @@ function previousSuggestion() {
     clickSuggestion4.style.opacity = "0";
     clickSuggestion5.style.opacity = "0"; //Прозрачность услайдеро
 
-    choiceimg1.style.backgroundImage= "url(../img/svg/a-heart.svg)";
-    choiceimg2.style.backgroundImage= "url(../img/svg/briefcase.svg)";
-    choiceimg3.style.backgroundImage= "url(../img/svg/leaf.svg)";
-    choiceimg4.style.backgroundImage= "url(../img/svg/rocket.svg)";
-    choiceimg5.style.backgroundImage= "url(../img/svg/umbrella.svg)";//Все иконки серые
+    choiceimg1.style.backgroundImage= "url(./img/svg/a-heart.svg)";
+    choiceimg2.style.backgroundImage= "url(./img/svg/briefcase.svg)";
+    choiceimg3.style.backgroundImage= "url(./img/svg/leaf.svg)";
+    choiceimg4.style.backgroundImage= "url(./img/svg/rocket.svg)";
+    choiceimg5.style.backgroundImage= "url(./img/svg/umbrella.svg)";//Все иконки серые
 
     suggestionEmage1.style.display = "none";
     suggestionEmage2.style.display = "none";
@@ -187,7 +55,7 @@ choiceClick.addEventListener("click", function (event) {
     if (event.target.closest(".choice__suggestion-1")) {
         previousSuggestion();
         clickSuggestion1.style.opacity = "1";
-        choiceimg1.style.backgroundImage = "url(../img/svg/a-heart-green.svg)";
+        choiceimg1.style.backgroundImage = "url(./img/svg/a-heart-green.svg)";
         choiceTitle[4].style = "";
         choiceTitle[3].style = "";
         choiceTitle[2].style = "";
@@ -199,7 +67,7 @@ choiceClick.addEventListener("click", function (event) {
     if (event.target.closest(".choice__suggestion-2")) {
         previousSuggestion();
         clickSuggestion2.style.opacity = "1";
-        choiceimg2.style.backgroundImage = "url(../img/svg/briefcase-green.svg)";
+        choiceimg2.style.backgroundImage = "url(./img/svg/briefcase-green.svg)";
         choiceTitle[4].style = "";
         choiceTitle[3].style = "";
         choiceTitle[2].style = "";
@@ -211,7 +79,7 @@ choiceClick.addEventListener("click", function (event) {
     if (event.target.closest(".choice__suggestion-3")) {
         previousSuggestion();
         clickSuggestion3.style.opacity = "1";
-        choiceimg3.style.backgroundImage = "url(../img/svg/leaf-green.svg)";
+        choiceimg3.style.backgroundImage = "url(./img/svg/leaf-green.svg)";
         choiceTitle[4].style = "";
         choiceTitle[3].style = "";
         choiceTitle[0].style = "";
@@ -223,7 +91,7 @@ choiceClick.addEventListener("click", function (event) {
     if (event.target.closest(".choice__suggestion-4")) {
         previousSuggestion();
         clickSuggestion4.style.opacity = "1";
-        choiceimg4.style.backgroundImage = "url(../img/svg/rocket-green.svg)";
+        choiceimg4.style.backgroundImage = "url(./img/svg/rocket-green.svg)";
         choiceTitle[4].style = "";
         choiceTitle[1].style = "";
         choiceTitle[2].style = "";
@@ -235,7 +103,7 @@ choiceClick.addEventListener("click", function (event) {
     if (event.target.closest(".choice__suggestion-5")) {
         previousSuggestion();
         clickSuggestion5.style.opacity = "1";
-        choiceimg5.style.backgroundImage = "url(../img/svg/umbrella-green.svg)";
+        choiceimg5.style.backgroundImage = "url(./img/svg/umbrella-green.svg)";
         choiceTitle[1].style = "";
         choiceTitle[3].style = "";
         choiceTitle[2].style = "";
@@ -246,8 +114,6 @@ choiceClick.addEventListener("click", function (event) {
 
 });
 
-
-// var rrr = document.
 
 
 
